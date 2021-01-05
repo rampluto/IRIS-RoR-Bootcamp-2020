0094f8ece2edffe2a63255c5539feb09c7376b57
Ram Janam Yadav <ramjanamjnvj1972@gmail.com>
2021-01-05 13:21:15 +0530

    solved the problems

--- session_0/fizz_buzz.rb
+++ session_0/fizz_buzz.rb
@@ -36,6 +36,22 @@
 #
 # We will raise an `ArgumentError` exception to let the caller know that
 # their function arguments were incorrect.
-def fizz_buzz(n:, x: 3, y: 5)
+def fizz_buzz(n:, x:3, y:5)
+  if(x<=0 or y<=0 or n<0)
+    raise ArgumentError.new("Function arguments are incorrect") #divisibility by 0 or -ve number is not possible
+  end
+  result = []
+  n.times do |time|
+      if (time+1)%x==0 and (time+1)%y==0
+        result[time]="FizzBuzz"
+      elsif (time+1)%x==0
+        result[time]="Fizz"
+      elsif (time+1)%y==0
+        result[time]="Buzz"
+      else result[time]=(time+1).to_s
+
+      end
+  end
+  return result
   raise NotImplementedError # TODO
 end
--- session_0/word_processing.rb
+++ session_0/word_processing.rb
@@ -2,13 +2,20 @@
 # any non-lower case alphabet (A..Z) to corresponding lower case
 # alphabet
 def lower_case(words)
-  raise NotImplementedError # TODO
+  result=[]
+  words.each do |word|
+    result.push(word.downcase)
+  end
+  return result
 end
 
 # Similar to `lower_case`, this function modifies the array in-place
 # and does not return any value.
 def lower_case!(words)
-  raise NotImplementedError # TODO
+  words.each do |word|
+    word.downcase!
+  end
+  return words
 end
 
 # Given a prefix and an array of words, return an array containing
@@ -18,7 +25,13 @@ end
 # words_with_prefix('apple', ['apple', 'ball', 'applesauce']) would
 # return the words 'apple' and 'applesauce'.
 def words_with_prefix(prefix, words)
-  raise NotImplementedError # TODO
+result=[]
+words.each do |word|
+  if word.include?(prefix)
+    result.push(word)
+  end
+end
+return result
 end
 
 # The similarity score between two words is defined as the length of
@@ -34,11 +47,11 @@ end
 # The function `similarity_score` takes two words and returns the
 # similarity score (an integer).
 def similarity_score(word_1, word_2)
-  raise NotImplementedError # TODO
+raise NotImplementedError
 end
 
 # Given a chosen word and an array of words, return an array of word(s)
 # with the maximum similarity score in the order they appear.
 def most_similar_words(chosen_word, words)
-  raise NotImplementedError # TODO
+raise NotImplementedError # TODO
 end
